af880c32d6b8b0ed345f57b36719d34603bd6efb
AkshatJangid787 <akshatjangid14@gmail.com>
2024-10-16 15:02:43 +0530

    Frontend of Navbar Completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <div className='w-full h-screen bg-white'>
+    <div className='w-full h-screen bg-gradient-to-b from-[#0B1120] via-[#0F172A] to-[#1E1B4B]'>
       <Navbar/>
     </div>
   )
